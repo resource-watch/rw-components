8a6ed579df7f62bf7fd9212e4db1856c9929e839
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-04-26 13:48:25 +0200

    making progress on the Vocabularies form and table

--- src/components/vocabularies/form/VocabulariesForm.jsx
+++ src/components/vocabularies/form/VocabulariesForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Autobind } from 'es-decorators';
+import sortBy from 'lodash/sortBy';
 
 import { STATE_DEFAULT } from './constants';
 
 import Button from '../../ui/Button';
 import VocabulariesTable from '../table/VocabulariesTable';
+import Spinner from '../../ui/Spinner';
 import { get, post } from '../../../utils/request';
 
 class VocabulariesForm extends React.Component {
@@ -13,6 +15,7 @@ class VocabulariesForm extends React.Component {
     super(props);
     const newState = Object.assign({}, STATE_DEFAULT, {
       loading: true,
+      vocabularies: [],
       form: Object.assign({}, STATE_DEFAULT.form, {
         application: props.application,
         authorization: props.authorization,
@@ -23,48 +26,65 @@ class VocabulariesForm extends React.Component {
     this.state = newState;
   }
 
+  componentDidMount() {
+    this.getVocabularies();
+  }
+
+  /**
+   * HELPERS
+   * - getVocabularies
+  */
+  getVocabularies() {
+    const url = 'https://api.resourcewatch.org/v1/vocabulary';
+
+    get({
+      url,
+      headers: [],
+      onSuccess: (response) => {
+        const vocabularies = sortBy(response.data.map(vocabulary =>
+          ({ name: vocabulary.id })
+        ), 'id');
+        this.setState({ vocabularies, loading: false });
+      },
+      onError: () => {
+        this.setState({ message: 'Error loading vocabularies', loading: false });
+      }
+    });
+  }
+
   /**
    * UI EVENTS
    * - triggerNewVocabulary
-   * - handleRemoveVocabulary
-   * - handleEditVocabulary
   */
   @Autobind
   triggerNewVocabulary() {
-    const { vocabularies } = this.state;
-    if (!vocabularies.find(voc => voc.name === '')) {
-      vocabularies.push({ name: '', tags: [] });
-      this.setState({ vocabularies });
-    }
+    
   }
-  // @Autobind
-  // handleRemoveVocabulary(voc) {
-  //
-  // }
-  // @Autobind
-  // handleEditVocabulary(voc) {
-  //
-  // }
 
   render() {
     const { application, authorization } = this.props;
+    const { vocabularies } = this.state;
     return (
       <div>
         <h1 className="-p-primary">
           Vocabularies
         </h1>
         {!this.state.loading &&
-          <Button
-            onClick={this.triggerNewVocabulary}
-            properties={{
-              type: 'button',
-              className: '-primary'
-            }}
-          >
-            New Vocabulary
-          </Button>
+          <div>
+            <Button
+              onClick={this.triggerNewVocabulary}
+              properties={{
+                type: 'button',
+                className: '-primary -end'
+              }}
+            >
+              New Vocabulary
+            </Button>
+          </div>
         }
+        <Spinner className="-light" isLoading={this.state.loading} />
         <VocabulariesTable
+          vocabularies={vocabularies}
           application={application}
           authorization={authorization}
         />
--- src/components/vocabularies/table/VocabulariesTable.jsx
+++ src/components/vocabularies/table/VocabulariesTable.jsx
@@ -1,62 +1,17 @@
 import React from 'react';
-import sortBy from 'lodash/sortBy';
-import Spinner from '../../ui/Spinner';
 import CustomTable from '../../ui/customtable/CustomTable';
 
 class VocabulariesTable extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      vocabularies: [],
-      loading: true
-    };
-  }
-
-  componentDidMount() {
-    this.getVocabularies();
-  }
-
-  /**
-   * HELPERS
-   * - getVocabularies
-  */
-  getVocabularies() {
-    const url = 'https://api.resourcewatch.org/v1/vocabulary';
-
-    fetch(new Request(url))
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error(response.statusText);
-      })
-      .then((response) => {
-        const vocabularies = sortBy(response.data.map(vocabulary =>
-          vocabulary.id
-        ), 'id');
-        this.setState({ vocabularies, loading: false });
-      })
-      .catch(() => {
-        this.setState({ message: 'Error loading vocabularies', loading: false });
-      });
-  }
-
   render() {
     return (
       <div className="c-vocabularies-table">
-        <Spinner className="-light" isLoading={this.state.loading} />
         <CustomTable
           columns={[
             { label: 'name', value: 'name' }
           ]}
-          actions={{
-            show: true,
-            list: [
-              // { name: 'Edit', path: '/datasets/:id/edit', show: true, component: EditAction },
-              // { name: 'Remove', path: '/datasets/:id/remove', component: DeleteAction, componentProps: { authorization: this.props.authorization } }
-            ]
-          }}
-          data={this.state.vocabularies}
+          actions={{ show: false, list: [] }}
+          data={this.props.vocabularies}
           pageSize={20}
           pagination={{
             enabled: true,
@@ -79,7 +34,8 @@ VocabulariesTable.defaultProps = {
 
 VocabulariesTable.propTypes = {
   application: React.PropTypes.array.isRequired,
-  authorization: React.PropTypes.string
+  authorization: React.PropTypes.string,
+  vocabularies: React.PropTypes.array
 };
 
 export default VocabulariesTable;
